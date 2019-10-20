82e1a21172c7c6b55db92d71e0700658d28a15a9
Wanchat <wanchatgame@gmail.com>
2019-10-20 14:57:32 +0700

    1. ใส่ ListView และสร้าง layout ชื่อ contact_detail
    2.
    ContactsContract
    แก้ setOnClickListener
    
    3.
    adapter ไปยัง layout

--- app/src/main/java/com/example/kotlintutorial_10_2/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial_10_2/MainActivity.kt
@@ -6,7 +6,9 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 private const val TAG = "MainActivity"
 
@@ -26,6 +28,16 @@ class MainActivity : AppCompatActivity() {
                 null,
                 null,
                 ContactsContract.Contacts.DISPLAY_NAME_PRIMARY)
+
+            val contacts = ArrayList<String>()
+            cursor?.use {
+                while (it.moveToNext()) {
+                    contacts.add(it.getString(it.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY)))
+                }
+            }
+
+            val adapter = ArrayAdapter<String>(this, R.layout.contact_detail, R.id.name, contacts)
+            contact_names.adapter = adapter
 
             Log.d(TAG, "fab onClick: ends")
         }
